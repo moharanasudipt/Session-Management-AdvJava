f7950cbbcf53bc05015c2a7e49865b0e19686058
moharanasudipt <moharanasudiptkumar@gmail.com>
2023-10-15 09:49:11 +0530

    second commit..

---
+++ src/java/NewServlet1.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class NewServlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    
+}
